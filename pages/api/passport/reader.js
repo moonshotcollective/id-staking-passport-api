f93fe3ac4a05bf7863eb25a111c880305dc75a92
farque65 <fahim.farque@gmail.com>
2022-08-16 17:57:16 -0400

    feat(app): adds fetchSignaturePayload function

--- pages/api/passport/reader.js
+++ pages/api/passport/reader.js
@@ -1,8 +1,6 @@
 import { PassportReader } from '@gitcoinco/passport-sdk-reader';
 // --- Crypto lib for hashing
 import { randomBytes } from 'crypto';
-// ---- Generate & Verify methods
-import * as DIDKit from '@spruceid/didkit-wasm-node';
 
 async function useReader(address, node) {
 	const reader = new PassportReader(node);
@@ -10,31 +8,51 @@ async function useReader(address, node) {
 	return passport;
 }
 
+// Fetch a verifiableCredential
+const fetchSignaturePayload = async (passport, signer) => {
+	// must provide signature for message
+	if (!signer) {
+		throw new Error('Unable to sign message without a signer');
+	}
+
+	// sign the challenge provided by the IAM
+	const signature =
+		passport && passport.expiryDate && passport.issuanceDate
+			? (await signer.signMessage(JSON.stringify(passport))).toString()
+			: '';
+
+	// must provide signature for message
+	if (!signature) {
+		throw new Error('Unable to sign message');
+	}
+
+	const nonce = randomBytes(16).toString('base64');
+	// return payload
+	return {
+		signature: signature,
+		timestamp: Date.now(),
+		nonce: nonce,
+	};
+};
+
 export default async function reader(req, res) {
 	const address = req.query.address?.toString().toLowerCase();
 	const signer = req.query.signer;
 
 	// Passport prod node is the default
 	const node = req.query.node || process.env.CERAMIC_CLIENT_URL;
-	const result = await useReader(address, node);
-	const key = process.env.IAM_JWK || DIDKit.generateEd25519Key();
+	const passport = await useReader(address, node);
 
 	let returnPayload = {};
-
-	// If the user has a passport then continue
-	if (key && result.expiryDate && result.issuanceDate) {
-		//Sign Message
-		const signature = await signer.signMessage(result);
-
-		const nonce = randomBytes(16).toString('base64');
-		returnPayload = {
-			signature: signature,
-			timestamp: Date.now(),
-			nonce: nonce,
-			error: '',
-		};
-	} else {
-		returnPayload.error = 'Passport Not Found';
+	try {
+		// If the user has a passport then continue
+		if (passport.expiryDate && passport.issuanceDate) {
+			returnPayload = await fetchSignaturePayload(passport, signer);
+		} else {
+			returnPayload = { error: 'Passport Not Found' };
+		}
+	} catch (e) {
+		console.error(`ERROR: ${e}`);
 	}
 
 	res.setHeader('Access-Control-Allow-Origin', '*');
