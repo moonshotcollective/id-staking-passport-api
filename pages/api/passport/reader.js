b9e20a20c1b6026ab19f15a504829e0a48187315
farque65 <fahim.farque@gmail.com>
2022-08-19 19:31:04 -0400

    feat(api): implements cors

--- pages/api/passport/reader.js
+++ pages/api/passport/reader.js
@@ -79,5 +79,30 @@ export default async function reader(req, res) {
 	}
 
 	res.setHeader('Access-Control-Allow-Origin', '*');
+
+	// Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+	res.setHeader(
+		'Access-Control-Allow-Origin',
+		'https://goerli-staking.surge.sh/'
+	);
+
+	// Request methods you wish to allow
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+	);
+
+	// Request headers you wish to allow
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-Requested-With,content-type'
+	);
+
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.setHeader('Access-Control-Allow-Credentials', true);
+
 	res.json(returnPayload);
 }
