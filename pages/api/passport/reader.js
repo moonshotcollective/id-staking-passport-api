b4b5328c7864e85707a60bdbe061ae3fd29c6f91
farque65 <fahim.farque@gmail.com>
2022-08-22 13:25:20 -0400

    feat(api): refactored api return payloads

--- pages/api/passport/reader.js
+++ pages/api/passport/reader.js
@@ -12,67 +12,66 @@ async function useReader(address) {
 export default async function reader(req, res) {
 	// user address
 	const address = req.query.address?.toString().toLowerCase();
-
+	//GET CHAIN ID for Signature domain, default to 1
+	const DOMAIN_CHAIN_ID = req.query.domainChainId?.toString().toLowerCase();
+	// payload returned by api
+	let returnPayload = {};
 	// Passport prod node is the default
 	const passport = await useReader(address);
-	let returnPayload = {};
+
+	// If the user has a passport then continue
+	if (!passport.expiryDate || !passport.issuanceDate) {
+		return res.status(403).json({ message: 'Passport Not Found' });
+	}
 
 	// initialize a wallet
 	const mnemonic = process.env.PRIVATE_KEY || '';
 	const account = ethers.Wallet.fromMnemonic(mnemonic);
-
 	const wallet = new ethers.Wallet(
 		account.privateKey,
 		ethers.getDefaultProvider()
 	);
 
 	if (!wallet || !wallet.privateKey) {
-		return (returnPayload = { error: 'Private key not found' });
+		return res.status(403).json({ message: 'Private key not found' });
 	}
 
 	try {
-		// If the user has a passport then continue
-		if (passport.expiryDate && passport.issuanceDate) {
-			// signature data
-			const nonce = randomBytes(16).toString('base64');
-			const timestamp = Date.now();
+		// signature data
+		const nonce = randomBytes(8).toString('base64');
+		const timestamp = Date.now();
 
-			// All properties on a domain are optional
-			let domain = {
-				name: 'IDStaking',
-				version: '1.0',
-				chainId: '1',
-				verifyingContract: process.env.CONTRACT_ADDRESS,
-			};
+		// All properties on a domain are optional
+		let domain = {
+			name: 'IDStaking',
+			version: '1.0',
+			chainId: DOMAIN_CHAIN_ID || '1',
+			verifyingContract: process.env.CONTRACT_ADDRESS,
+		};
 
-			// The named list of all type definitions
-			const types = {
-				Data: [
-					{ name: 'passport', type: 'string' },
-					{ name: 'timestamp', type: 'string' },
-					{ name: 'nonce', type: 'string' },
-					{ name: 'user', type: 'address' },
-				],
-			};
+		// The named list of all type definitions
+		const types = {
+			Data: [
+				{ name: 'timestamp', type: 'string' },
+				{ name: 'nonce', type: 'string' },
+				{ name: 'user', type: 'address' },
+			],
+		};
 
-			// The data to sign
-			const value = {
-				passport: JSON.stringify(passport),
-				timestamp: timestamp,
-				nonce: nonce,
-				user: address,
-			};
+		// The data to sign
+		const value = {
+			timestamp: timestamp,
+			nonce: nonce,
+			user: address,
+		};
 
-			const signature = await wallet._signTypedData(domain, types, value);
+		const signature = await wallet._signTypedData(domain, types, value);
 
-			returnPayload = {
-				signature: signature,
-				timestamp: timestamp,
-				nonce: nonce,
-			};
-		} else {
-			returnPayload = { error: 'Passport Not Found' };
-		}
+		returnPayload = {
+			signature: signature,
+			timestamp: timestamp,
+			nonce: nonce,
+		};
 	} catch (e) {
 		console.error(`ERROR: ${e}`);
 	}
